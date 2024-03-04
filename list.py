ee63990b1f05b613dcc9a72fadfd7437a0c9dc1d
BalaMuruganPy <155520046+BalaMuruganPy@users.noreply.github.com>
2024-03-04 20:00:21 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,7 @@
+l=[1,3,4,5,"ffff",45,67,16]
+print(len(l))
+print(l[0:2])
+print(l[::1])
+print(f"the elelment{l[0::3]}")
+print("the element",l[0:5:3])
+
---
+++ list1.py
@@ -0,0 +1,6 @@
+name=["a","b","c","d"]
+name=("a","b","c","d")
+total=len(name)
+print(total)
+number=random.randint(0,total)
+print(name[number])
